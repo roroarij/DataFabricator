8883161baf6ccd85d6fd9c81c34e0b1c7d3cfcb4
roroarij <rosenarij@gmail.com>
2023-05-10 21:51:44 -0400

    Update query.py

--- database/query.py
+++ database/query.py
@@ -7,7 +7,7 @@ class QueryGenerator:
         {"role": "system", "content": "You are a helpful Data Engineering assistant."},
         {"role": "user", "content": f"Output a valid json array with the keys query and description, containing 10 SQLite queries useful to perform analysis of our data based on the following schema: \n{schema}"}
     ]
-      print("Generating query ideas...")
+      print("Generating query ideas..")
       query_ideas_array = self.chat_api.generate_chat_completion(messages, model="gpt-4", temperature=0.5)
       print(query_ideas_array)
       query_ideas_array=[i.get('query') for i in query_ideas_array]
